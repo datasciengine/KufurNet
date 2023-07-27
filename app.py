a62678658668873949693818da69896b1fc4ba6a
MSAHIN <murat.sahin@normdigital.com>
2023-07-27 10:21:16 +0300

    Add all initial files.

---
+++ app.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from kufur_net import KufurNet
+
+import uvicorn
+
+app = FastAPI()
+kufurnet = KufurNet()
+
+
+@app.post("/profanity_checker")
+async def profanity_checker(comment: dict):
+    """
+    :param comment: {'comment' : 'hay amk'}
+    :return:
+    """
+
+    return kufurnet.get_swears(comment)
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=9000)
