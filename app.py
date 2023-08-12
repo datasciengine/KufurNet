bd1317d471579ce700c4821372c73c42de03c048
MSAHIN <murat.sahin@normdigital.com>
2023-08-12 22:18:43 +0300

    Update return, input and validations.

--- app.py
+++ app.py
@@ -7,14 +7,32 @@ app = FastAPI()
 kufurnet = KufurNet()
 
 
-@app.post("/analyze_profanity")
-async def analyze_profanity(text: dict):
+@app.post("/check_profanity")
+async def check_profanity(input_message: dict):
     """
-    :param text: {'text' : 'hay amk'}
+    :param input_message: {'text' : 'hay amk'}
     :return:
     """
-    return kufurnet.analyze(text)
+
+    kufurnet.validate_input(input_message)
+
+    text = input_message["text"]
+    is_black, black_list, score = kufurnet.filter_analyze(text)
+
+    return {
+        "status": True,
+        "result": {"is_black": is_black,
+                   "black_list": black_list,
+                   "score": score}}
 
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=9000)
+
+    """
+        1. BERT Modeli, 100 yorum, ort. 6 saniye.
+        2. (Sinkaf + Toygar) SVM Modeli, 100 yorum, ms mertebesinde.
+        3. Toygar(SVM) Modeli, 100 yorum, ms mertebesinde.
+        4. Throff Veri (SVM) Modeli, 100 yorum ms mertebesinde.
+        5. Filter Analyze (Rule Based)
+    """
